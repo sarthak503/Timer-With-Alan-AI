1c8cfdd3d77434a7497879bb6f0454a64b09243c
sarthak503 <sarthak.kumar503@gmail.com>
2021-06-06 21:19:39 +0530

    Added JS and app is now working

---
+++ index.js
@@ -0,0 +1,43 @@
+let x = document.getElementById('input-field');
+let hours=document.getElementById('hours');
+let minutes=document.querySelector('#minutes');
+let seconds =document.querySelector('#seconds');
+let totTime,handle;
+
+
+function reset()
+{
+hours.innerText = '0';
+minutes.innerText = '0';
+seconds.innerText = '0';
+x.value = "";
+clearInterval(handle)
+document.getElementById('start').disabled=false;
+}
+function startTimer()
+{
+totTime = parseInt(x.value);
+handle = setInterval(timer,1000);
+document.getElementById('start').disabled=true;
+}
+function timer()
+{
+formatTime(totTime);
+
+if(totTime<=0){
+reset();
+}
+else{
+totTime--;
+}
+}
+function formatTime(second)
+{
+    const hh = parseInt(second/3600);
+    second = second%3600;
+    const mm = parseInt(second/60);
+    second = second % 60;
+    hours.innerText = hh;
+    minutes.innerText = mm;
+    seconds.innerText = second;
+}
