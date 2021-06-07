7dd261b6e539bc89669724d9bcf85a9c3f078696
sarthak503 <sarthak.kumar503@gmail.com>
2021-06-07 23:53:19 +0530

    added a feature for error msg

--- index.js
+++ index.js
@@ -3,22 +3,47 @@ let hours=document.getElementById('hours');
 let minutes=document.querySelector('#minutes');
 let seconds =document.querySelector('#seconds');
 let totTime,handle;
-
+let er;
+function isnan()
+{
+    var x
+    x=document.getElementById("input-field").value;
+    
+    if(x==''){
+        er="Input Invalid";
+                     
+    }
+    else{
+        er=""
+        startTimer();
+    }
+    document.getElementById("demo").innerHTML = er;
+      
+}
+function changetxt()
+{
+    er="";
+}
 
 function reset()
 {
-hours.innerText = '0';
-minutes.innerText = '0';
-seconds.innerText = '0';
+er="";
+document.getElementById("demo").innerHTML = er;
+hours.innerText = '00';
+minutes.innerText = '00';
+seconds.innerText = '00';
 x.value = "";
 clearInterval(handle)
 document.getElementById('start').disabled=false;
 }
 function startTimer()
 {
+
 totTime = parseInt(x.value);
+
 handle = setInterval(timer,1000);
 document.getElementById('start').disabled=true;
+
 }
 function beep() {
     var snd = new Audio(o=(A=new AudioContext()).createOscillator());
@@ -28,7 +53,6 @@ function beep() {
 function timer()
 {
 formatTime(totTime);
-
 if(totTime<=0){
 reset();
 beep();
